6d9100dbe8c663131a9eccfb0115d0083f19db20
PRASADREDDIVALLURU <valluruprasadreddy@gmail.com>
2024-01-08 21:48:06 +0530

    8-jan-24

---
+++ Phase2/Assistedpracticeprojects-1/ServletClassesInterfaces/src/main/java/InterfaceDemo.java
@@ -0,0 +1,38 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebServlet;
+
+@WebServlet("/InterfaceDemo")
+public class InterfaceDemo implements Servlet{
+	ServletConfig config=null;
+	 public void init(ServletConfig config){
+	 this.config=config;
+	 System.out.println("Initialization complete");
+	 }
+	 public void service(ServletRequest req,ServletResponse res)
+	 throws IOException,ServletException{
+	 res.setContentType("text/html");
+	 PrintWriter pwriter=res.getWriter();
+	 pwriter.print("<html>");
+	 pwriter.print("<body>");
+	 pwriter.print("In the service() method<br>");
+	 pwriter.print("</body>");
+	 pwriter.print("</html>");
+	 }
+	 public void destroy(){
+	 System.out.println("In destroy() method");
+	 }
+	 public ServletConfig getServletConfig(){
+	 return config;
+	 }
+	 public String getServletInfo(){
+	 return "This is a sample servlet info";
+	 }
+
+}
---
+++ Phase2/Assistedpracticeprojects-1/ServletCookies/src/main/java/LoginServlet.java
@@ -0,0 +1,45 @@
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.annotation.*;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LoginServlet() {
+        // TODO Auto-generated constructor stub
+    	super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		Cookie userId = new Cookie("userid",request.getParameter("userid"));
+				 response.addCookie(userId);
+				 response.sendRedirect("dashboard");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
